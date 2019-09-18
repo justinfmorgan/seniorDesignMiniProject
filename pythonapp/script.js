b22c1573bc9bd2d4d60b3f9e61458225be51cf7c
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2019-09-17 17:43:29 -0700

    Update script.js

--- pythonapp/script.js
+++ pythonapp/script.js
@@ -1,8 +1,18 @@
-function onSignIn(googleUser) {
-    "use strict";
-    var profile = googleUser.getBasicProfile();
-    $(".g-signin2").css("display", "none");//hide sign in buttton
-    $(".data").css("display", "block");//diplay profile data
-    $("#profile_picture").attr('src',profile.getImageUrl());//display profile picture
-    $("#email").text(profile.getEmail());//display email text information
+function create_table_vals() {
+    var rand_temp_val= Math.floor(Math.random()*20);
+    var td_1 = document.createElement("td");
+    td_1.innerHTML = rand_temp_val;
+
+    var rand_humd_val= Math.floor(Math.random()*20);
+    var td_2 = document.createElement("td");
+    td_2.innerHTML = rand_humd_val;
+
+    var tbl = document.getElementById("table1");
+    var row = document.createElement("tr");
+
+    row.appendChild(td_1);
+    row.appendChild(td_2);
+    tb1.appendChild(row);
+
+    $(".table_data").css("display","block");
 }
